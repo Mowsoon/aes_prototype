2e426aeedf353515f2bdcc1bb42cfa813cdfbe5e
Mowsoon <alban.munari@gmail.com>
2025-01-29 19:43:16 +0100

    moving gen_key128

--- aes_128.c
+++ aes_128.c
@@ -8,3 +8,23 @@ void init_key_128(const uint32_t words[WORDS_128], uint32_t round_key[KEYS_128][
         }
     }
 }
+
+void gen_keys128(AES_KEY* key, uint32_t round_key[KEYS_128][KEY_SIZE]) {
+    int i;
+    uint32_t words[WORDS_128];
+    for (i = 0; i < 4; i++) {
+        words[i] = (key->key[i * 4] << 24) | (key->key[i * 4 + 1] << 16) |
+                   (key->key[i * 4 + 2] << 8) | key->key[i * 4 + 3];
+    }
+
+    for (i = 4 ; i < WORDS_128; i++) {
+        if (i % 4 == 0) {
+            words[i] = words[i - 4] ^ sub_word(rotate_left(words[i - 1])) ^ RCON[i/4];
+        }
+        else {
+            words[i] = words[i - 4] ^ words[i - 1];
+        }
+    }
+    init_key_128(words, round_key);
+
+}
