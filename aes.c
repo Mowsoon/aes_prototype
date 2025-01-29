ac90caa727fa3d69455ee01ddb64c93bad30f588
Mowsoon <alban.munari@gmail.com>
2025-01-29 17:34:16 +0100

    simplifying code

--- aes.c
+++ aes.c
@@ -5,10 +5,8 @@ void generate_aes_key_struct(AES_KEY* key, size_t key_len) {
         printf("Key size must be 128 or 192 or 256\n");
         exit(1);
     }
-    else {
-        key->key_size = key_len;
-        for (size_t i = 0; i < key_len / 8; i++) {
-            key->key[i] = rand() % 256;
-        }
+    key->key_size = key_len;
+    for (size_t i = 0; i < key_len / 8; i++) {
+        key->key[i] = rand() % 256;
     }
 }
