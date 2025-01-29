8e762ffb178b332bad3145c7aafc45235eac007b
Mowsoon <alban.munari@gmail.com>
2025-01-29 18:52:55 +0100

    generating every word for 128

--- aes.c
+++ aes.c
@@ -36,8 +36,15 @@ uint32_t rotate_left(uint32_t word) {
     return (word << 8) | (word >> 24);
 }
 
-void aes128(AES_KEY* key) {
-    uint32_t words[KEYS_128];
+uint32_t sub_word(uint32_t word) {
+    uint8_t *byte = (uint8_t *)&word;
+    for (int i = 0; i < 4; i++) {
+        byte[i] = SBOX[byte[i]];
+    }
+    return word;
+}
+
+void gen_word128(AES_KEY* key, uint32_t words[KEYS_128]) {
     int i;
 
     for (i = 0; i < 4; i++) {
@@ -45,5 +52,21 @@ void aes128(AES_KEY* key) {
                    (key->key[i * 4 + 2] << 8) | key->key[i * 4 + 3];
     }
 
+    for (i = 4 ; i < KEYS_128; i++) {
+        if (i % 4 == 0) {
+            words[i] = words[i - 4] ^ sub_word(rotate_left(words[i - 1])) ^ RCON[i/4];
+        }
+        else {
+            words[i] = words[i - 4] ^ words[i - 1];
+        }
+    }
+}
 
+void aes128(AES_KEY* key) {
+    uint32_t words[KEYS_128];
+    gen_word128(key, words);
+
+    for (int i = 0; i < KEYS_128; i++) {
+        printf("Word %d: %08x\n", i, words[i]);
+    }
 }
